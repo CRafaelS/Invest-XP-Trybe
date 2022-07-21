c068809bb52c34bba4b5b562be6ea84675469a37
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-07-21 11:57:38 -0300

    refactor: refatorando os nomes no BD para fazer a querry

--- invest_xp_trybe.sql
+++ invest_xp_trybe.sql
@@ -7,7 +7,7 @@ USE Invest_XP_Trybe ;
 -- Table Client`
 -- -----------------------------------------------------
 CREATE TABLE Client (
-  idClient INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  codCliente INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   email VARCHAR(45) NOT NULL UNIQUE,
   password VARCHAR(45) NOT NULL
@@ -22,25 +22,25 @@ INSERT INTO Client(name, email, password)
 CREATE TABLE Account(
   idAccount INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   balance DECIMAL(15,2),
-  idClient INT NOT NULL,
+  codCliente INT NOT NULL,
   dateMov TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (idClient) REFERENCES Client(idClient)
+  FOREIGN KEY (codCliente) REFERENCES Client(codCliente)
   );
 
-INSERT INTO Account(balance, idClient) 
+INSERT INTO Account(balance, codCliente) 
   VALUES (2000.00, 1);
 
 -- -----------------------------------------------------
 -- Table Assets
 -- -----------------------------------------------------
 CREATE TABLE Assets (
-  idAsset INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  assetCod VARCHAR(45) NOT NULL UNIQUE,
-  quantity INT ,
+  codAtivo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  assetCod  VARCHAR(45) NOT NULL UNIQUE,
+  qtdeAtivo INT ,
   price DECIMAL(10,2)
   );
 
-INSERT INTO Assets(assetCod, quantity, price) 
+INSERT INTO Assets(assetCod, qtdeAtivo, price) 
   VALUES ('XPBR31', 750000, 100.51),
   ('ITUB4', 700000, 23.72),
   ('WEGE3', 650000, 25.99),
@@ -51,14 +51,14 @@ INSERT INTO Assets(assetCod, quantity, price)
 -- Table ClientAssets
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS ClientAssets (
-  idClient INT NOT NULL,
-  idAsset INT NOT NULL,
-  quantityAsset INT,
-  CONSTRAINT PRIMARY KEY (idClient, idAsset),
-  FOREIGN KEY (idClient) REFERENCES Client(idClient),
-  FOREIGN KEY (idAsset) REFERENCES Assets(idAsset)
+  codCliente INT NOT NULL,
+  codAtivo INT NOT NULL,
+  qtdeAtivo INT,
+  CONSTRAINT PRIMARY KEY (codCliente, codAtivo),
+  FOREIGN KEY (codCliente) REFERENCES Client(codCliente),
+  FOREIGN KEY (codAtivo) REFERENCES Assets(codAtivo)
   );
 
-  INSERT INTO ClientAssets(idClient, idAsset, quantityAsset) 
+  INSERT INTO ClientAssets(codCliente, codAtivo, qtdeAtivo) 
     VALUES (1, 1, 500),
     (1,3, 300);
